7e7376a6e0aebfde6657a2b1896662968ce65c49
sefagkdmr <52085784+sefagkdmr@users.noreply.github.com>
2023-01-03 00:24:26 +0300

    heheehe siuu

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,14 +2,32 @@
 let sozler = [
     "Bilginin efendisi olmak için çalışmanın uşağı olmak şarttır. (Balzac)",
     "Nerede olursanız olun, elinizdekilerle yapabileceklerinizi yapın. (Alex Morrison)",
-    "Başarısız olmak zordur, ancak başaramaya çalışmamış olmak daha da zordur. (Theodore Roosevelt)",
+    "Başarısız olmak zordur, ancak başarmaya çalışmamış olmak daha da zordur. (Theodore Roosevelt)",
 ]
 let soz = sozler[Math.floor(Math.random() * sozler.length)]
 
 document.getElementById("rand-text").innerHTML = soz;
 
-
-
+//
+yonlendir = function() {
+    window.location = "#";
+}
+var y;
+$(document).ready(function() {
+    $("#ucgen").addClass("fa fa-plus")
+    y = 0;
+})
+$(".icon-bars").click(function() {
+    if(y == 0) {
+        $("#ucgen").removeClass("fa fa-plus")
+        $("#ucgen").toggleClass("fa fa-times")
+        y = 1;
+    } else {
+        $("#ucgen").removeClass("fa fa-times")
+        $("#ucgen").addClass("fa fa-plus")
+        y = 0;
+    }
+})
 
 if ($(".text-slider").length == 1) {
     var typed_strings = $(".text-slider-items").text();
